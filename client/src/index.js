94a39f768d988d4a3d251a2fa9bfd95b05e183ce
Julian Leal <julianleal1997@gmail.com>
2022-08-01 12:19:42 -0500

    Front end up but major error

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+  <React.StrictMode>
+    <Router >
+      <App />
+    </Router>
+  </React.StrictMode>
 );
 
 reportWebVitals();
